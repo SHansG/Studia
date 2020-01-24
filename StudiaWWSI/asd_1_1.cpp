381c7a9a218a1a71eb0a4b4336f983ff923660dd
SHansG <47045312+SHansG@users.noreply.github.com>
2020-01-24 11:43:12 +0100

    Create asd_1_1.cpp

---
+++ StudiaWWSI/asd_1_1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	int liczba = 1;
+	int maxliczba = 0;
+	while (liczba != 0)
+	{
+		cin >> liczba;
+		if (maxliczba < liczba)
+		{
+			maxliczba = liczba;
+		}
+	}
+	cout << maxliczba << endl;
+
+	return 0;
+}
